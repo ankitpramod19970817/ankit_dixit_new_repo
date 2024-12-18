f90bc73b671e2d1736771fda37df8be91234ace1
ankitpramod19970817 <168293534+ankitpramod19970817@users.noreply.github.com>
2024-12-18 10:54:02 +0530

    Update Twosum2.java

--- src/main/java/org/example/leetcode/Twosum2.java
+++ src/main/java/org/example/leetcode/Twosum2.java
@@ -13,10 +13,10 @@ public class Twosum2 {
         ans[0]=ans[1]=-1;
         HashMap<Integer,Integer> hashMap=new HashMap<>();
         for (int i=0;i< nums.length;i++) {
-            int g = nums[i];
-            int r = target - g;
+            int g = nums[i];//7
+            int r = target - g;//2
             if (hashMap.containsKey(r)) {
-                ans[0] = nums[i];//7
+                ans[0] = hashmap.get(r);//0
                 ans[1] = i;//1
                 System.out.println(ans);//
                 return ans;
